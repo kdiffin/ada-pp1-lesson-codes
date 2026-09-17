68dd695a6c6e29968926e10daa1db516c9ef364c
aslkb <badalzadekamal@gmail.com>
2026-09-17 16:29:54 +0400

    week-3

---
+++ week-3/char-manipulation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char grade = 'A';
+    char gr = 'y';
+    char digit = '5';
+
+    //printf("%d\n", digit); - This would give a error because digit is a character, not an integer
+    printf("%c\n", digit);
+    return 0;
+}
