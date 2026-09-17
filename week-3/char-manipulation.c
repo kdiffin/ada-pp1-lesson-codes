1c7f8552057d0571beff4a1f7eba7c87a5172bff
aslkb <badalzadekamal@gmail.com>
2026-09-17 16:51:58 +0400

    week-3 nuke

--- week-3/char-manipulation.c
+++ week-3/char-manipulation.c
@@ -5,7 +5,19 @@ int main() {
     char gr = 'y';
     char digit = '5';
 
+    
     //printf("%d\n", digit); - This would give a error because digit is a character, not an integer
-    printf("%c\n", digit);
+    // for fun you can remove comments and see what happens, but use gcc -Wall to see the warning
+    printf("%c\n", digit); // This will print the CHAR '5', not an integer
+    printf("               test\n"); // This will print test with spaces before it, as spaces is also a character
+
+    printf("now enter two chars separeated by space: ");
+    scanf("%c %c", &grade, &gr);
+    printf("You entered: %c and %c\n", grade, gr); //works as expected, right?
+
+    printf("now do it again: ");
+    scanf("%c%c", &grade, &gr);
+    printf("You entered: %c and %c\n", grade, gr); //spaces are important, if you don't put a space between the %c's, it will not work as expected
+    
     return 0;
 }
---
+++ week-3/logic.c
@@ -0,0 +1,29 @@
+#include  <stdio.h>
+
+int main(void) {
+    int age = 18;
+
+    printf("%d\n greater than 18: ", age > 18); // This will print 0 because age is not greater than 18
+    printf("%d\n less than 18: ", age < 18); // This will print 0 because age is not less than 18
+    printf("%d\n equal to 18: ", age == 18); // This will print 1 because age is equal to 18
+    printf("%d\n greater than or equal to 18: ", age >= 18); // This will print 1 because age is greater than or equal to 18
+    printf("%d\n less than or equal to 18: ", age <= 18); // This will print 1 because age is less than or equal to 18
+    printf("%d\n not equal to 18: ", age != 18); // This will print 0 because age is not equal to 18
+    // so, 0 is FALSE and 1 is TRUE
+
+    // if,elif,else statement
+    if (age > 18) {
+        printf("You are older than 18\n");
+    } else if (age < 18) {
+        printf("You are younger than 18\n");
+    } else {
+        printf("You are 18 years old\n");
+    }
+
+    // in this case nothing will be printed because age is not greater than 18, so the if statement will not be executed
+    if (age > 18) {
+        printf("You are older than 18\n");
+    }
+
+    return 0;
+}
